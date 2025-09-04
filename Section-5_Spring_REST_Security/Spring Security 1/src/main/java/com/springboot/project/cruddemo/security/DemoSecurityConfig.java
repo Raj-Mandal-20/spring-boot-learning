7e4eb22dd8d10b65326c12a9e5678ee7aedc5fb4
Raj-Mandal-20 <usingrajcseinfo@gmail.com>
2025-09-04 15:18:55 +0530

    spring security noop auth , this is not used in production environment mainly used for testing

---
+++ Section-5_Spring_REST_Security/Spring Security 1/src/main/java/com/springboot/project/cruddemo/security/DemoSecurityConfig.java
@@ -0,0 +1,58 @@
+package com.springboot.project.cruddemo.security;
+
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class DemoSecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails jhon = User.builder()
+                .username("jhon")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("MANAGER","EMPLOYEE")
+                .build();
+
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(jhon, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configure ->
+            configure.requestMatchers(HttpMethod.GET, "/api/members").hasRole("EMPLOYEE")
+                     .requestMatchers(HttpMethod.GET, "/api/members/**").hasRole("EMPLOYEE")
+                     .requestMatchers(HttpMethod.POST, "/api/members").hasAnyRole("MANAGER")
+                     .requestMatchers(HttpMethod.PUT, "/api/members").hasRole("MANAGER")
+                     .requestMatchers(HttpMethod.DELETE, "/api/members").hasRole("ADMIN")
+                     .requestMatchers(HttpMethod.DELETE, "/api/members/**").hasRole("ADMIN"));
+
+        http.httpBasic();
+        // http.httpBasic();  // depricated
+
+        // CSRF is not required for REST API
+         http.csrf().disable(); // depricated
+
+        return http.build();
+
+    }
+
+}
